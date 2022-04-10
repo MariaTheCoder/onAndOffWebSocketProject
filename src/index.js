f581b1da0e4b1718c05b46dd3149f7645a39b824
Maria T. Andersen <mteilmann93@gmail.com>
2022-04-10 18:02:28 +0200

    serve html files to according routes

--- src/index.js
+++ src/index.js
@@ -2,10 +2,19 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const server = http.createServer(app);
+const path = require("path");
 const port = 3000;
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello world</h1>");
+});
+
+app.get("/light", (req, res) => {
+  res.sendFile(path.join(__dirname, "html", "light.html"));
+});
+
+app.get("/switch", (req, res) => {
+  res.sendFile(path.join(__dirname, "html", "switch.html"));
 });
 
 server.listen(3000, () => {
